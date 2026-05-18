2d121f0d5dd0daa51e869ac39588b26a0e24f1a5
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-05-19 04:56:09 +0700

    sumshit in the 5 AM

---
+++ src/alrNbaIKnowUTooLazyTooOpenACalc/LimbusCompanySpent.java
@@ -0,0 +1,39 @@
+package alrNbaIKnowUTooLazyTooOpenACalc;
+
+import java.util.Scanner;
+
+public class LimbusCompanySpent {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your Lunacy: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // consume leftover newline after nextInt()
+
+        int pulls = n / 1300;
+        int leftover = n % 1300;
+
+        System.out.printf(
+            "You have %d 10-pull(s) to spend, with %d Lunacy left over.%n",
+            pulls, leftover
+        );
+
+        System.out.printf(
+            "Would you like to know how many pulls you could get with %d Lunacy left over?%n"
+            + "( Y / N ) : ",
+            leftover
+        );
+
+        String ans = sc.nextLine();
+        if (ans.equalsIgnoreCase("Y")) {
+            int pulls2 = leftover / 130;
+            int leftover2 = leftover % 130;
+            System.out.printf(
+                "You have %d pull(s) to spend, with %d Lunacy left over.%n",
+                pulls2, leftover2
+            );
+        }
+
+        sc.close();
+    }
+}
